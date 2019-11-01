7e130dee81991606be66c58a8ee983572ed9e73f
Samantha Plesce <samplesce@gmail.com>
2019-10-31 21:25:48 -0400

    Fixed nested folder.

--- FileIO.cpp
+++ FileIO.cpp
@@ -6,19 +6,59 @@
  */
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
 
+using namespace KP_FileIO;
 using namespace std;
 
-int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
+//trys to open filename, and return its contents in contents
+	//it will strip out only \n's (char return\line feed)
+	//returns:
+	//SUCCESS all worked, contents string contains all in the file
+	//COULD_NOT_OPEN_FILE_TO_READ contents string is cleared
+int getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	ifstream file;
+	file.open(filename.c_str(), ios::in);
+	if (!file.is_open())
+		return COULD_NOT_OPEN_FILE_TO_READ;
+
+	string line;
+	while (!file.eof()){
+		getline(file, line);
+		contents += line;
+	}
+	file.close();
+	if (contents.empty())
+		return COULD_NOT_OPEN_FILE_TO_READ;
+
+	return SUCCESS;
 }
 
-int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
+//trys to open filename, and serialize  myEntryVector contents to it
+	//returns:
+	//SUCCESS all worked, filename contains myEntryVector strings
+	//COULD_NOT_OPEN_FILE_TO_WRITE
+int writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	int i = 0
+	fstream file;
+	file.open(filename, ios::out);
+
+	if (!file.is_open())
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+
+	while (i < myEntryVector.size()){
+		file << myEntryVector[i];
+	}
+	if (myEntryVector.empty())
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+
+
+	file.close();
+	return SUCCESS;
 }
 
 
