95705392603c12d026e37660f18f38ab86370aee
Guest <samplesce@gmail.com>
2019-11-01 10:59:06 -0400

    done

--- FileIO.cpp
+++ FileIO.cpp
@@ -10,30 +10,29 @@
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
 
-using namespace KP_FileIO;
 using namespace std;
+using namespace KP_FileIO;
 
 //trys to open filename, and return its contents in contents
 	//it will strip out only \n's (char return\line feed)
 	//returns:
 	//SUCCESS all worked, contents string contains all in the file
 	//COULD_NOT_OPEN_FILE_TO_READ contents string is cleared
-int getFileContents(const std::string &filename, std::string &contents)
+int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
 	ifstream file;
-	file.open(filename.c_str(), ios::in);
-	if (!file.is_open())
+	file.open(filename);
+	if (!file.is_open()){
 		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
 
 	string line;
-	while (!file.eof()){
-		getline(file, line);
-		contents += line;
-	}
-	file.close();
-	if (contents.empty())
-		return COULD_NOT_OPEN_FILE_TO_READ;
 
+	while (getline(file, line)){
+		contents+=line;
+	}
+
+	file.close();
 	return SUCCESS;
 }
 
@@ -41,23 +40,21 @@ int getFileContents(const std::string &filename, std::string &contents)
 	//returns:
 	//SUCCESS all worked, filename contains myEntryVector strings
 	//COULD_NOT_OPEN_FILE_TO_WRITE
-int writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
+int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	int i = 0;
-	fstream file;
-	file.open(filename, ios::out);
-
-	if (!file.is_open())
+	ofstream file;
+	file.open(filename);
+	if (!file.is_open()){
 		return COULD_NOT_OPEN_FILE_TO_WRITE;
-
-	while (i < myEntryVector.size()){
-		file << myEntryVector[i];
 	}
-	if (myEntryVector.empty())
-		return COULD_NOT_OPEN_FILE_TO_WRITE;
 
+	vector<string>::iterator myStringVectorIterator;
+	for(myStringVectorIterator = myEntryVector.begin();
+			myStringVectorIterator != myEntryVector.end();
+			myStringVectorIterator++){
+		file << *myStringVectorIterator + "\n";
+	}
 
-	file.close();
 	return SUCCESS;
 }
 
